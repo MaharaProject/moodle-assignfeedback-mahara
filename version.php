2bc153fa62a984bb910f26389fce41887cec0673
Aaron Wells <aaronw@catalyst.net.nz>
2015-02-10 16:01:30 +1300

    Updating version.php for 2.7

--- version.php
+++ version.php
@@ -14,12 +14,14 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$plugin->version = 2013061200;
-$plugin->requires = 2012062500;
-$plugin->release = '1.0rc (Build: 2013061200)';
-$plugin->maturity = MATURITY_RC;
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2014051200;
+$plugin->requires = 2014051200;
 $plugin->component = 'assignfeedback_mahara';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0-mdl2.7';
 
 $plugin->dependencies = array(
-    'assignsubmission_mahara' => 2014082000
+    'assignsubmission_mahara' => 2014110501
 );
